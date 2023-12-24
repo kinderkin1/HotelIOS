361b347d6df48d6da7f979d4095db141ac1e5810
Kinder <94771655+kinderkin1@users.noreply.github.com>
2023-12-24 17:00:50 +0300

    Initial Commit

---
+++ TestUI/TestUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUIApp.swift
+//  TestUI
+//
+//  Created by Kin Der on 24.12.23.
+//
+
+import SwiftUI
+
+@main
+struct TestUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
